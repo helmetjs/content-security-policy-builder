c1c3af920822a6456e2289f9f5c52dd1648614d1
Ben James <benhjames@sky.com>
2019-06-13 18:53:06 +0100

    Convert module to TypeScript
    See [#7](https://github.com/helmetjs/content-security-policy-builder/pull/7).
    
    * Convert module to Typescript
    
    * Remove reference to standard
    
    * Add dist folder to gitignore

---
+++ index.ts
@@ -0,0 +1,37 @@
+function dashify (str: string) {
+  return str
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .toLowerCase();
+}
+
+interface PolicyBuilderOptions {
+  directives: { [directive: string]: string[] | string | boolean };
+}
+
+export = function ({ directives }: PolicyBuilderOptions): string {
+  const keysSeen: { [directive: string]: boolean } = {};
+
+  return Object.keys(directives).reduce<string[]>((result, originalKey) => {
+    const directive = dashify(originalKey);
+
+    if (keysSeen[directive]) {
+      throw new Error(`${originalKey} is specified more than once`);
+    }
+    keysSeen[directive] = true;
+
+    let value = directives[originalKey];
+    if (Array.isArray(value)) {
+      value = value.join(' ');
+    } else if (value === true) {
+      value = '';
+    } else if (value === false) {
+      return result;
+    }
+
+    if (value) {
+      return result.concat(`${directive} ${value}`);
+    } else {
+      return result.concat(directive);
+    }
+  }, []).join('; ');
+}
