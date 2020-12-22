e052c8fa604c231658b2664075f584bf12349c15
Evan Hahn <me@evanhahn.com>
2020-12-22 11:07:45 -0600

    Add Prettier and upgrade devDependencies

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
-function dashify (str: string) {
-  return str
-    .replace(/([a-z])([A-Z])/g, '$1-$2')
-    .toLowerCase();
+function dashify(str: string) {
+  return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 }
 
 interface PolicyBuilderOptions {
@@ -11,27 +9,29 @@ interface PolicyBuilderOptions {
 export = function ({ directives }: PolicyBuilderOptions): string {
   const keysSeen: { [directive: string]: boolean } = {};
 
-  return Object.keys(directives).reduce<string[]>((result, originalKey) => {
-    const directive = dashify(originalKey);
+  return Object.keys(directives)
+    .reduce<string[]>((result, originalKey) => {
+      const directive = dashify(originalKey);
 
-    if (keysSeen[directive]) {
-      throw new Error(`${originalKey} is specified more than once`);
-    }
-    keysSeen[directive] = true;
+      if (keysSeen[directive]) {
+        throw new Error(`${originalKey} is specified more than once`);
+      }
+      keysSeen[directive] = true;
 
-    let value = directives[originalKey];
-    if (Array.isArray(value)) {
-      value = value.join(' ');
-    } else if (value === true) {
-      value = '';
-    } else if (value === false) {
-      return result;
-    }
+      let value = directives[originalKey];
+      if (Array.isArray(value)) {
+        value = value.join(" ");
+      } else if (value === true) {
+        value = "";
+      } else if (value === false) {
+        return result;
+      }
 
-    if (value) {
-      return result.concat(`${directive} ${value}`);
-    } else {
-      return result.concat(directive);
-    }
-  }, []).join('; ');
-}
+      if (value) {
+        return result.concat(`${directive} ${value}`);
+      } else {
+        return result.concat(directive);
+      }
+    }, [])
+    .join("; ");
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,116 +1,111 @@
-import builder = require('..');
+import builder = require("..");
 
-describe('builder', () => {
-  it('builds no directives', () => {
+describe("builder", () => {
+  it("builds no directives", () => {
     const result = builder({ directives: {} });
 
-    expect(result).toStrictEqual('');
+    expect(result).toStrictEqual("");
   });
 
-  it('builds directives with camelCased keys', () => {
+  it("builds directives with camelCased keys", () => {
     const result = builder({
       directives: {
-        whatThe: 'heck',
+        whatThe: "heck",
         defaultSrc: "'self'",
-        playtimeIsOver: ['star', 'fox'],
+        playtimeIsOver: ["star", "fox"],
       },
     });
 
-    const split = result.split('; ').sort();
+    const split = result.split("; ").sort();
     expect(split).toStrictEqual([
       "default-src 'self'",
-      'playtime-is-over star fox',
-      'what-the heck',
+      "playtime-is-over star fox",
+      "what-the heck",
     ]);
   });
 
-  it('builds directives with dash-separated keys', () => {
+  it("builds directives with dash-separated keys", () => {
     const result = builder({
       directives: {
-        'do-a': 'barrel roll',
-        'default-src': "'self'",
-        'andross-has-ordered-us': ['to', 'take', 'you', 'down'],
+        "do-a": "barrel roll",
+        "default-src": "'self'",
+        "andross-has-ordered-us": ["to", "take", "you", "down"],
       },
     });
 
-    const split = result.split('; ').sort();
+    const split = result.split("; ").sort();
     expect(split).toStrictEqual([
-      'andross-has-ordered-us to take you down',
+      "andross-has-ordered-us to take you down",
       "default-src 'self'",
-      'do-a barrel roll',
+      "do-a barrel roll",
     ]);
   });
 
-  it('builds directives with a mix of key types', () => {
+  it("builds directives with a mix of key types", () => {
     const result = builder({
       directives: {
-        'hey-einstein': "i'm on your side",
+        "hey-einstein": "i'm on your side",
         defaultSrc: "'self'",
-        falco: ['lombardi'],
+        falco: ["lombardi"],
       },
     });
 
-    const split = result.split('; ').sort();
+    const split = result.split("; ").sort();
     expect(split).toStrictEqual([
       "default-src 'self'",
-      'falco lombardi',
+      "falco lombardi",
       "hey-einstein i'm on your side",
     ]);
   });
 
-  it('builds directives with weird keys', () => {
+  it("builds directives with weird keys", () => {
     const result = builder({
       directives: {
-        'lots--of----dashes': 'wow',
-        'ALLCAPS': 'YELLING',
-        'InotALWAYScapsNOPE': 'ok',
+        "lots--of----dashes": "wow",
+        ALLCAPS: "YELLING",
+        InotALWAYScapsNOPE: "ok",
       },
     });
 
-    const split = result.split('; ').sort();
+    const split = result.split("; ").sort();
     expect(split).toStrictEqual([
-      'allcaps YELLING',
-      'inot-alwayscaps-nope ok',
-      'lots--of----dashes wow',
+      "allcaps YELLING",
+      "inot-alwayscaps-nope ok",
+      "lots--of----dashes wow",
     ]);
   });
 
-  it('builds directives with empty values', () => {
+  it("builds directives with empty values", () => {
     const result = builder({
       directives: {
-        these: '',
+        these: "",
         are: [],
-        empty: [''],
+        empty: [""],
         values: true,
       },
     });
 
-    const split = result.split('; ').sort();
-    expect(split).toStrictEqual([
-      'are',
-      'empty',
-      'these',
-      'values',
-    ]);
+    const split = result.split("; ").sort();
+    expect(split).toStrictEqual(["are", "empty", "these", "values"]);
   });
 
-  it('does not include directives if the value is false', () => {
+  it("does not include directives if the value is false", () => {
     const result = builder({
       directives: {
-        included: 'yes',
+        included: "yes",
         skipped: false,
       },
     });
 
-    expect(result).toStrictEqual('included yes');
+    expect(result).toStrictEqual("included yes");
   });
 
-  it('throws errors when passed two keys of different types but the same names', () => {
+  it("throws errors when passed two keys of different types but the same names", () => {
     expect(() => {
       builder({
         directives: {
           defaultSrc: "'self'",
-          'default-src': 'falco.biz',
+          "default-src": "falco.biz",
         },
       });
     }).toThrow();
